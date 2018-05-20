0f8e4e456eb32abc901ee3c04b23cfa70319996c
Janice Darikho <janicedarikho@gmail.com>
2018-05-20 14:48:46 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def my_collect(array)
+  i = 0
+  final_array = []
+  while i < array.length
+    final_array << yield(array[i])
+    i += 1
+  end
+  final_array
+end
 
-
+my_collect(array) do |x| 
+end
